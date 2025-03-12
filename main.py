dfc3914ffe1862b94d35e5a1c2ebce21dad63c42
Cronk98 <andrewcronk98@gmail.com>
2025-03-12 13:26:51 -0400

    Set FPS to 60

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
           f"Screen height: {SCREEN_HEIGHT}"
           )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -19,6 +21,8 @@ def main():
     
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
